c64f330a525798b5b25827bbb59f7531a9a0d779
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2022-05-01 23:18:31 +0200

    Import material modules for input and form fields

--- libs/shared-domain/material-ui/src/lib/material-ui.module.ts
+++ libs/shared-domain/material-ui/src/lib/material-ui.module.ts
@@ -10,6 +10,8 @@ import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 const materialModules = [
   MatGridListModule,
@@ -21,6 +23,8 @@ const materialModules = [
   MatToolbarModule,
   MatListModule,
   MatTableModule,
+  MatFormFieldModule,
+  MatInputModule,
 ];
 
 @NgModule({
