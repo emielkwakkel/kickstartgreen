7e31607be8aaf01ce06cb48f2f615b7ab7211728
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2022-05-01 14:38:12 +0200

    Use PlantTrees and LatestOrders component for dashboard

--- libs/trees-domain/dashboard-feature/src/lib/dashboard/dashboard.component.ts
+++ libs/trees-domain/dashboard-feature/src/lib/dashboard/dashboard.component.ts
@@ -1,32 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
-import {
-  OverviewUiComponent,
-  LatestTransactionsUiComponent,
-} from '@banking/dashboard-ui';
+import { PlantTreesComponent } from '@trees/plant-ui';
 import { Observable } from 'rxjs';
 import { DashboardCard } from '@shared/base-ui/src/lib/dashboard/dashboard.component';
-import { gql, Apollo } from 'apollo-angular';
-
-const getOrdersQuery = gql`
-  query orders($accountId: String) {
-    orders(limit: 5, accountId: $accountId) {
-      id
-      orderPlacedAt
-      handle
-      message
-      reference
-    }
-  }
-`;
-
+import { LatestOrdersUiComponent } from '@trees/dashboard-ui/src';
 @Component({
   selector: 'trees-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
   /** Based on the screen size, switch from standard to one column per row */
   cards: Observable<DashboardCard[]> = this.breakpointObserver
     .observe(Breakpoints.Handset)
@@ -34,45 +17,22 @@ export class DashboardComponent implements OnInit {
       map(({ matches }) => {
         return [
           {
-            title: 'Overview',
-            component: OverviewUiComponent,
+            title: 'Plant Trees',
+            component: PlantTreesComponent,
             data: {},
             cols: 2,
-            rows: 0.5,
-          },
-          {
-            title: 'Payment: Latest',
-            component: LatestTransactionsUiComponent,
-            data: { account: 'payment' },
-            cols: matches ? 2 : 1,
             rows: 1,
           },
           {
-            title: 'Savings: Latest',
-            component: LatestTransactionsUiComponent,
-            data: { account: 'savings' },
-            cols: matches ? 2 : 1,
+            title: 'Latest orders',
+            component: LatestOrdersUiComponent,
+            data: {},
+            cols: 2,
             rows: 1,
           },
         ];
       })
     );
 
-  constructor(
-    private breakpointObserver: BreakpointObserver,
-    private apollo: Apollo
-  ) {}
-
-  ngOnInit(): void {
-    this.apollo
-      .watchQuery<any>({
-        query: getOrdersQuery,
-        variables: {
-          accountId: '1ffb5b0a-7dec-4ef9-a14f-0c1573259bdc',
-        },
-      })
-      .valueChanges.subscribe(({ data, loading }) => {
-        console.log(data, loading);
-      });
-  }
+  constructor(private breakpointObserver: BreakpointObserver) {}
 }
